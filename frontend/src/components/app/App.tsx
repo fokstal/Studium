f12e2e97102d4a82e690c3e010d0b2d5c9df6209
alexander <okyxxxf@gmail.com>
2024-04-11 10:34:48 +0300

    fix: add index ts files and remove useless components from app

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,14 +1,8 @@
-import { Button } from "../ui-kit/button/button";
 import "./default.css";
 
 export function App() {
   return (
     <div className="App">
-      <div style={{display: "flex", gap: "10px", margin: "30px"}}>
-      <Button>Go lbutton</Button>
-      <Button>Go lbutton</Button>
-      <Button disabled>Disabled</Button>
-      </div>
     </div>
   );
 }
