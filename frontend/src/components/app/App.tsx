915079c031c0c4ed456894d3df80559488a6c364
alexander <okyxxxf@gmail.com>
2024-04-15 20:22:23 +0300

    fix: remove components from app

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,12 +1,8 @@
-import { StudentsTable } from "../data-tables";
-import { Header } from "../header";
 import "./default.css";
 
 export function App() {
   return (
     <div className="App">
-      <Header/>
-      <StudentsTable/>
     </div>
   );
 }
