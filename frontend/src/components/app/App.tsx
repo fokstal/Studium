33b36dbbb249c5c84d1c6a3af7cf59bcc4f9f856
alexander <okyxxxf@gmail.com>
2024-04-15 22:00:21 +0300

    feat: add react router

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,9 +1,20 @@
+import { PageLayout } from "../../pages/PageLayout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./default.css";
 
 export function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<>Home</>}/>
+        <Route path="/" element={<PageLayout/>}>
+          <Route path="journal" element={<>Эл журнал</>}/>
+          <Route path="students" element={<>Студенты</>}/>
+          <Route path="students/:id" element={<>Студент</>}/>
+          <Route path="profile" element={<>Профиль</>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,22 +1,42 @@
 import { Flex } from "@chakra-ui/react";
 import { BaseLayout } from "../../layouts";
-import { Select, Avatar, Heading1 } from "../ui-kit";
-import { languages } from "../ui-kit/variables";
+import { Select, Avatar, Text } from "../ui-kit";
+import { colors, languages } from "../ui-kit/variables";
 import { AiOutlineEye } from "react-icons/ai";
+import { Link, useLocation } from "react-router-dom";
 
 export function Header() {
+  const location = useLocation();
+
   return (
     <BaseLayout>
       <Flex align="center" justify="space-between" h="100%">
-        <img src={process.env.PUBLIC_URL + "/images/logo.svg"} alt="Studium" width="158" height="30"/>
-        <Flex>
-          {/*TODO add links after add react router*/}
-          <Heading1>LINKS</Heading1>
+        <Link to="/">
+          <img src={process.env.PUBLIC_URL + "/images/logo.svg"} alt="Studium" width="158" height="30"/>
+        </Link>
+        <Flex gap="20px">
+          <Text>
+            <Link style={
+              {color: location.pathname.includes("/students") ? colors.green : colors.black, textDecoration: "none"}} 
+              to="/students">
+                Список учащихся
+            </Link>
+          </Text>
+          <Text>|</Text>
+          <Text>
+            <Link style={
+              {color: location.pathname.includes("/journal") ? colors.green : colors.black, textDecoration: "none"}}
+              to="/journal">
+                Электронный журнал
+            </Link>
+          </Text>
         </Flex>
         <Flex gap="20px" align="center">
           <AiOutlineEye size="24"/>
           <Select options={languages} placeholder="RU"/>
-          <Avatar/>
+          <Link to="/profile">
+            <Avatar/>
+          </Link>
         </Flex>
       </Flex>
     </BaseLayout>
