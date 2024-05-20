cf4172986e2658d901a4937ad6823bfaf0c1c87f
Fokstal <klabu2016@mail.ru>
2024-05-20 11:50:44 +0300

    style: rename Service to Repository in value (Mon 20.05.24 11:50 AM)

--- api/Services/PermissionService.cs
+++ api/Services/PermissionService.cs
@@ -3,11 +3,11 @@ using api.Repositories.Data;
 
 namespace api.Services
 {
-    public class PermissionService(UserRepository userService)
+    public class PermissionService(UserRepository userRepository)
     {
         public async Task<HashSet<PermissionEnum>> GetPermissionListAsync(int userId)
         {
-            return await userService.GetPermissionListAsync(userId);
+            return await userRepository.GetPermissionListAsync(userId);
         }
     }
 }
