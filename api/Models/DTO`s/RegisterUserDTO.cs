b1b0f05766132052a85e5bbea232e2358a2b23f3
Fokstal <klabu2016@mail.ru>
2024-05-21 17:30:33 +0300

    feat: add required params to props on FML names (Tue 21.05.24 5:30 PM)

--- api/Models/DTO`s/RegisterUserDTO.cs
+++ api/Models/DTO`s/RegisterUserDTO.cs
@@ -8,9 +8,19 @@ namespace api.Model.DTO
         [Required]
         [Length(3, 10)]
         public string Login { get; set; } = null!;
+
+        [Required]
+        [Length(3, 15)]
         public string FirstName { get; set; } = null!;
+
+        [Required]
+        [Length(3, 25)]
         public string MiddleName { get; set; } = null!;
+
+        [Required]
+        [Length(3, 20)]
         public string LastName { get; set; } = null!;
+
 
         [Required]
         [Length(10, 15)]
