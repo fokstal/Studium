79e5e418a35f44a361b769759d2e6e34f1da6186
Fokstal <klabu2016@mail.ru>
2024-05-21 17:12:23 +0300

    fix: rename prop to correct (Tue 21.05.24 5:12 PM)

--- api/Models/Entities/SubjectEntity.cs
+++ api/Models/Entities/SubjectEntity.cs
@@ -3,7 +3,7 @@ namespace api.Models
     public class SubjectEntity : ModelEntity
     {
         public string Name { get; set; } = null!;
-        public string Descripton { get; set; } = null!;
+        public string Description { get; set; } = null!;
         public Guid TeacherId { get; set; }
 
         public int? GroupId { get; set; }
