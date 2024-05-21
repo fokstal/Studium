fe436f396c5b380143371fb8f65f38c7bab711f4
Fokstal <klabu2016@mail.ru>
2024-05-21 16:21:11 +0300

    feat: generate Guid by Role in configurations (Tue 21.05.24 4:21 PM)

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be53c288f02e8588b2e90bebd1a61faa1e0d0406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a093fd543ecaf5c543b19258db5dce57b03bd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
