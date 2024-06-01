5484ef67a58930fad8e0bad444a6cc0935483d13
Fokstal <klabu2016@mail.ru>
2024-06-01 23:14:31 +0300

    db: insert data (Sat 01.06.24 11:14 PM)

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e670bababea97bd18b9892df591428119b6a28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854be7ebc9ee20c7bfc3227543a60e0cc1090ca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
