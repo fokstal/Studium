8b168aa267f63500d57d953deca3547545b5b3e2
alexander <okyxxxf@gmail.com>
2024-06-01 17:53:33 +0300

    feat: create confirmation for delete subjects and groups

--- frontend/src/components/groups/GroupTable.tsx
+++ frontend/src/components/groups/GroupTable.tsx
@@ -3,9 +3,24 @@ import { TableWrapper } from "../data-tables";
 import { Group } from "../../types";
 import { Column } from "primereact/column";
 import { colors } from "../ui-kit";
-import { Box, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+} from "@chakra-ui/react";
+import { Button } from "../ui-kit";
 import { FaEdit, FaPlus, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { AiOutlineCloseSquare } from "react-icons/ai";
+import { LanguageContext, Translator } from "../../store";
 import { GroupService } from "../../services";
 
 type GroupTableProps = {
@@ -15,58 +30,114 @@ type GroupTableProps = {
 const groupService = new GroupService();
 
 export function GroupTable({ data }: GroupTableProps) {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [groupToDelete, setGroupToDelete] = useState<number>(0);
+  const { lang } = useContext(LanguageContext);
+
   const deleteGroupHandler = (id: number) => {
-    groupService.delete(id);
+    setGroupToDelete(id);
+    setIsDeleteModalOpen(true);
+  };
+
+  const confirmDeleteGroup = () => {
+    groupService.delete(groupToDelete);
+    setIsDeleteModalOpen(false);
     window.location.reload();
   };
 
+  const cancelDeleteGroup = () => {
+    setIsDeleteModalOpen(false);
+    setGroupToDelete(0);
+  };
+
   return (
-    <TableWrapper>
-      <DataTable
-        value={data}
-        resizableColumns
-        paginator={data.length > 7}
-        rows={7}
-      >
-        <Column field="name" header="Название" sortable></Column>
-        <Column field="description" header="Описание" sortable></Column>
-        <Column field="auditoryName" header="Аудитория" sortable></Column>
-        <Column
-          body={(rowData) => (
-            <Flex gap="10px" align="center">
-              <Box
-                onClick={() => deleteGroupHandler(rowData.id)}
-                p="5px"
-                bg={colors.red}
-                borderRadius="5px"
-                cursor="pointer"
-              >
-                <FaTrash color={colors.white} />
-              </Box>
-              <Box
-                p="5px"
-                bg={colors.green}
-                borderRadius="5px"
-                cursor="pointer"
-              >
-                <Link to={`/group/edit/${rowData.id}`}>
-                  <FaEdit color={colors.white} />
-                </Link>
-              </Box>
-              <Box
-                p="5px"
-                bg={colors.green}
-                borderRadius="5px"
-                cursor="pointer"
-              >
-                <Link to="/group/new">
-                  <FaPlus color={colors.white} />
-                </Link>
-              </Box>
-            </Flex>
-          )}
-        ></Column>
-      </DataTable>
-    </TableWrapper>
+    <>
+      <TableWrapper>
+        <DataTable
+          value={data}
+          resizableColumns
+          paginator={data.length > 7}
+          rows={7}
+        >
+          <Column
+            field="name"
+            header={Translator[lang.name]["group_name"]}
+            sortable
+          ></Column>
+          <Column
+            field="description"
+            header={Translator[lang.name]["group_description"]}
+            sortable
+          ></Column>
+          <Column
+            field="auditoryName"
+            header={Translator[lang.name]["group_auditory"]}
+            sortable
+          ></Column>
+          <Column
+            body={(rowData) => (
+              <Flex gap="10px" align="center">
+                <Box
+                  onClick={() => deleteGroupHandler(rowData.id)}
+                  p="5px"
+                  bg={colors.red}
+                  borderRadius="5px"
+                  cursor="pointer"
+                >
+                  <FaTrash color={colors.white} />
+                </Box>
+                <Box
+                  p="5px"
+                  bg={colors.green}
+                  borderRadius="5px"
+                  cursor="pointer"
+                >
+                  <Link to={`/group/edit/${rowData.id}`}>
+                    <FaEdit color={colors.white} />
+                  </Link>
+                </Box>
+                <Box
+                  p="5px"
+                  bg={colors.green}
+                  borderRadius="5px"
+                  cursor="pointer"
+                >
+                  <Link to="/group/new">
+                    <FaPlus color={colors.white} />
+                  </Link>
+                </Box>
+              </Flex>
+            )}
+          ></Column>
+        </DataTable>
+      </TableWrapper>
+      <Modal isOpen={isDeleteModalOpen} onClose={cancelDeleteGroup} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+            display="flex"
+            justifyContent="flex-end"
+            alignItems="center"
+          >
+            <AiOutlineCloseSquare
+              cursor="pointer"
+              fontSize="24px"
+              color={colors.darkGreen}
+              onClick={cancelDeleteGroup}
+            />
+          </ModalHeader>
+          <ModalBody justifyContent="center">
+            <Text fontSize="24px" fontWeight="bold" textAlign="center">
+              {Translator[lang.name]["confirm_delete"]}?
+            </Text>
+          </ModalBody>
+          <ModalFooter justifyContent="center">
+            <Button onClick={confirmDeleteGroup}>
+              {Translator[lang.name]["confirm_delete"]}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
