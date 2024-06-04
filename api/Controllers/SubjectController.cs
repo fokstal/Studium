9cd8e6a9890c15ec08f4b81fab5f7d334eaeeb5a
Fokstal <klabu2016@mail.ru>
2024-06-04 21:12:34 +0300

    fix: require permission to get List (Tue 04.06.24 9:12 PM)

--- api/Controllers/SubjectController.cs
+++ api/Controllers/SubjectController.cs
@@ -23,7 +23,7 @@ namespace api.Controllers
         [HttpGet("list")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [RequirePermissions([ViewSubject])]
+        [RequirePermissions([ViewSubjectList])]
         public async Task<ActionResult<IEnumerable<SubjectEntity>>> GetListAsync()
             => Ok(await _subjectRepository.GetListAsync());
 
