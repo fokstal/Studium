98c7e9a2d5031f9da8f7c2733b02b5d65234c7b9
Fokstal <klabu2016@mail.ru>
2024-06-04 18:26:42 +0300

    fix: update methods from PictureRepo (Tue 04.06.24 6:26 PM)

--- api/Repositories/Data/PassportRepository.cs
+++ api/Repositories/Data/PassportRepository.cs
@@ -8,18 +8,31 @@ namespace api.Repositories.Data
 {
     public class PassportRepository(AppDbContext db) : DataRepositoryBase<PassportEntity, PassportDTO>(db)
     {
+        private readonly PersonRepository _personRepository = new(db);
         public override PassportEntity Create(PassportDTO passportDTO)
         {
+            PersonEntity personEntity =
+                _personRepository.GetAsync(personEntityId: passportDTO.PersonEntityId).Result
+                ?? throw new Exception("Person on Passport is null!");
+
             return new()
             {
-                ScanFileName = UploadPassportScanAsync(passportDTO.ScanFile).Result,
+                ScanFileName =
+                    UploadPassportScanAsync(passportDTO.ScanFile, personEntity.FirstName + personEntity.AvatarFileName).Result,
+                    
                 PersonEntityId = passportDTO.PersonEntityId,
             };
         }
 
         public override async Task UpdateAsync(PassportEntity passportEntityToUpdate, PassportDTO passportDTO)
         {
-            passportEntityToUpdate.ScanFileName = await UploadPassportScanAsync(passportDTO.ScanFile);
+            PersonEntity personEntity =
+                _personRepository.GetAsync(personEntityId: passportDTO.PersonEntityId).Result
+                ?? throw new Exception("Person on Passport is null!");
+
+            passportEntityToUpdate.ScanFileName = await
+                UploadPassportScanAsync(passportDTO.ScanFile, personEntity.FirstName + personEntity.AvatarFileName);
+
             passportEntityToUpdate.PersonEntityId = passportDTO.Id;
 
             await _db.SaveChangesAsync();
